49b5c490f52b722293a1ea0e6c907c3ea3e12da4
Kamila Semotánová <kamila.semotanova@gmail.com>
2023-06-10 18:01:25 +0200

    Add ChemCircle component

---
+++ src/components/ChemCircle/index.jsx
@@ -0,0 +1,18 @@
+import React, { useState } from 'react';
+import './style.css';
+import {chemicals} from "../../chemicals";
+import { Chemical } from '../Chemical';
+
+export const ChemCircle = () => {
+  return chemicals.map((chemical)=>(<Chemical 
+        id={chemical.id} 
+        url={chemical.url} 
+        name={chemical.nameOfProduct} 
+        chemical={chemical.nameOfChemical} 
+        products={chemical.otherProducts} 
+        use={chemical.use} 
+        specs={chemical.specification} 
+        warnings={chemical.warningSign}/>
+  )
+ )
+} 
