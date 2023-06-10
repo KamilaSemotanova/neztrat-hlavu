1085b010e52c7207f9cf9d65ab5151ecde36922c
Kamila Semotánová <kamila.semotanova@gmail.com>
2023-06-10 09:32:21 +0200

    initial commit

---
+++ index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './style.css';
+
+const App = () => {
+  return (
+    <>
+      <h1>Neztrať hlavu!</h1>
+    </>
+  );
+};
+
+createRoot(document.querySelector('#app')).render(<App />);
