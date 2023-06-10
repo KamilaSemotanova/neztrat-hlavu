12da9d31481564a46d22c8402b4dcc3d83809a34
Kamila Semotánová <kamila.semotanova@gmail.com>
2023-06-10 20:21:55 +0200

    Secure communication between parent and child

--- src/components/ChemCircle/index.jsx
+++ src/components/ChemCircle/index.jsx
@@ -2,19 +2,55 @@ import React, { useState } from 'react';
 import './style.css';
 import { chemicals } from '../../chemicals';
 import { Chemical } from '../Chemical';
+import { MixCircle } from '../MixCircle';
 
 export const ChemCircle = () => {
-  return chemicals.map((chemical) => (
-    <Chemical
-      key={chemical.id}
-      id={chemical.id}
-      url={chemical.url}
-      name={chemical.nameOfProduct}
-      chemical={chemical.nameOfChemical}
-      products={chemical.otherProducts}
-      use={chemical.use}
-      specs={chemical.specification}
-      warnings={chemical.warningSign}
-    />
-  ));
+  const [draggedElement, setDraggedElement] = useState();
+
+  const handleOnTouchStart = (id) => {
+    setDraggedElement(id);
+  };
+
+  const handleOnTouchMove = (event) => {
+    const positionX = event.touches[0].pageX;
+    const positionY = event.touches[0].pageY;
+  };
+
+  const handleOnTouchEnd = (event) => {
+    const finalPosition = document.elementFromPoint(
+      event.changedTouches[0].pageX,
+      event.changedTouches[0].pageY,
+    );
+    if (finalPosition.classList.value === 'mixCircle') {
+      console.log(finalPosition);
+    }
+  };
+
+  return (
+    <>
+      {chemicals.map((chemical) => (
+        <Chemical
+          key={chemical.id}
+          id={chemical.id}
+          url={chemical.url}
+          name={chemical.nameOfProduct}
+          chemical={chemical.nameOfChemical}
+          products={chemical.otherProducts}
+          use={chemical.use}
+          specs={chemical.specification}
+          warnings={chemical.warningSign}
+          onTouchStart={() => {
+            handleOnTouchStart(chemical.id);
+          }}
+          onTouchMove={(event) => {
+            handleOnTouchMove(event);
+          }}
+          onTouchEnd={(event) => {
+            handleOnTouchEnd(event);
+          }}
+        />
+      ))}
+      <MixCircle />
+    </>
+  );
 };
--- src/components/Chemical/index.jsx
+++ src/components/Chemical/index.jsx
@@ -2,17 +2,44 @@ import React, { useState } from 'react';
 import './style.css';
 import { InfoBox } from '../InfoBox';
 
-export const Chemical = ({id, url, name, chemical, products, use, specs, warnings }) => {
-  const[openInfoBox, setOpenInfoBox]= useState(false) 
-  
+export const Chemical = ({
+  id,
+  url,
+  name,
+  chemical,
+  products,
+  use,
+  specs,
+  warnings,
+  onTouchStart,
+  onTouchMove,
+  onTouchEnd,
+}) => {
+  const [openInfoBox, setOpenInfoBox] = useState(false);
+
   const handleClick = () => {
-  setOpenInfoBox(!openInfoBox)
-  }
+    setOpenInfoBox(!openInfoBox);
+  };
 
   return (
-    <div onClick={handleClick} >
-      <img src={url} alt="product picture" className='chemical' />
-      {openInfoBox && <InfoBox id={id} name={name} chemical={chemical} products={products} use={use} specs={specs} warnings={warnings}/>}
+    <div
+      onClick={handleClick}
+      onTouchStart={onTouchStart}
+      onTouchMove={onTouchMove}
+      onTouchEnd={onTouchEnd}
+    >
+      <img src={url} alt="product picture" className="chemical" />
+      {openInfoBox && (
+        <InfoBox
+          id={id}
+          name={name}
+          chemical={chemical}
+          products={products}
+          use={use}
+          specs={specs}
+          warnings={warnings}
+        />
+      )}
     </div>
-  )
-}
+  );
+};
