bfa994633cd8504557066e95507dfd3fed14d742
Kamila Semotánová <kamila.semotanova@gmail.com>
2023-06-10 14:09:03 +0200

    Add footer with links

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -5,6 +5,8 @@ export const Footer = () => {
   return (
     <footer>
       <p>© 2023 Neztrať hlavu</p>
+      <a href="https://github.com/KamilaSemotanova/neztrat-hlavu" target="_blank">Repozitář hry</a>
+      <a href='https://about-neztrathlavu.netlify.app/' target="_blank">Stránka o hře: </a>
     </footer>
   );
 };
