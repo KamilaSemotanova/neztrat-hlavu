d15d46629f28aacab7b41bf43f3cdd7ac4d70111
Kamila Semotánová <kamila.semotanova@gmail.com>
2023-06-10 14:10:24 +0200

    Connect about with header

---
+++ src/components/Header/About/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './style.css';
+
+export const About = () => {
+  console.log("ahoj")
+  return (
+  <div className='infoAbout'>
+    <h1>Neztrať hlavu!</h1>
+    <p>Kombinujte chemikálie a zjistěte, co z nich vznikne.</p>
+  </div>)
+}
