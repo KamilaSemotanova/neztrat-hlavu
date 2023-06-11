eb60d31fe040470ee1f2a4623ef1e54b38ed0552
Susanne Krkoska <zuzka.krkoska@seznam.cz>
2023-06-11 11:19:05 +0200

    Add ChemDropdown WIP

--- src/components/ChemDropdown/index.jsx
+++ src/components/ChemDropdown/index.jsx
@@ -1,27 +1,85 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import './style.css';
-import {chemicals} from "../../chemicals";
-import {InfoBox} from "../InfoBox";
-import {MixResult} from "../MixResult";
-
+import { chemicals } from '../../chemicals';
+import { InfoBox } from '../InfoBox';
+import { MixResult } from '../MixResult';
 
 export const ChemDropdown = () => {
   const [select1, setSelect1] = useState();
   const [select2, setSelect2] = useState();
   const [chemicalInfo, setChemicalInfo] = useState();
   const [mix, setMix] = useState(false);
+  const [open, setOpen] = useState(false);
   const handleOpenInfoBox = (select) => {
-    const selectedId = select==="select1" ? select1 : select2;
-    const selectedChemical = chemicals.find((chemical) => chemical.id===selectedId);
+    const selectedId = select === 'select1' ? select1 : select2;
+    const selectedChemical = chemicals.find(
+      (chemical) => chemical.id === selectedId,
+    );
     setChemicalInfo(selectedChemical);
-    console.log(selectedChemical)
-  }
+    console.log(selectedChemical);
+  };
 
   const handleSubmit = () => {
-    event.preventDefault()
+    event.preventDefault();
     console.log(select1);
     setMix(!mix);
-  }
+  };
+
+  return (
+    <>
+      <div onClick={() => setOpen(!open)}>Vyberte první chemikálii:</div>
+      {open ? (
+        <div className="chembox__grid container">
+          <div className="chembox__button" value="">
+            <img
+              className="chembox__icon"
+              src={require('../../img/rocket.svg')}
+            />
+          </div>
+          {chemicals.map((chemical) => (
+            <div className={select2 === chemical.id ? "chembox__button disabled" : "chembox__button"}
+              key={chemical.id}
+              value={chemical.id}
+              onClick={() => {
+                setSelect1(chemical.id);
+                console.log(select1)
+              }}
+            >
+              <img className="chembox__icon" src={chemical.url} />
+              <div>{chemical.id}</div>
+            </div>
+          ))}
+        </div>
+      ) : null}
+
+      <div onClick={() => setOpen(!open)}>Vyberte druhou chemikálii:</div>
+      {open ? (
+          <div className="chembox__grid container">
+            <div className="chembox__button" value="">
+              <img
+                  className="chembox__icon"
+                  src={require('../../img/rocket.svg')}
+              />
+            </div>
+            {chemicals.map((chemical) => (
+                <div
+                    className={select1 === chemical.id ? "chembox__button disabled" : "chembox__button"}
+                    key={chemical.id}
+                    value={chemical.id}
+                    disabled={select1 === chemical.id ? true : false}
+                    onClick={() => {
+                      setSelect2(chemical.id);
+                      console.log(select2)
+                    }}
+                >
+                  <img className="chembox__icon" src={chemical.url} />
+                  <div>{chemical.id}</div>
+                </div>
+            ))}
+          </div>
+      ) : null}
+    </>
+  );
 
   return (
     <div className="seznam-chemikalii">
@@ -33,9 +91,25 @@ export const ChemDropdown = () => {
             </div>
             <select onChange={(evt) => setSelect1(evt.target.value)}>
               <option value="">Vyberte</option>
-              {chemicals.map((chemical) => <option key={chemical.id} value={chemical.id} disabled={select2===chemical.id ? true : false}>{chemical.nameOfProduct}</option>)};
+              {chemicals.map((chemical) => (
+                <option
+                  key={chemical.id}
+                  value={chemical.id}
+                  disabled={select2 === chemical.id ? true : false}
+                >
+                  {chemical.nameOfProduct}
+                </option>
+              ))}
+              ;
             </select>
-            <button className="btn" type="button" disabled={select1 ? false : true} onClick={() => handleOpenInfoBox("select1")}>?</button>
+            <button
+              className="btn"
+              type="button"
+              disabled={select1 ? false : true}
+              onClick={() => handleOpenInfoBox('select1')}
+            >
+              ?
+            </button>
           </label>
           <label>
             <div className="seznam-chemikalii-label">
@@ -43,16 +117,47 @@ export const ChemDropdown = () => {
             </div>
             <select onChange={(evt) => setSelect2(evt.target.value)}>
               <option value="">Vyberte</option>
-              {chemicals.map((chemical) => <option key={chemical.id} value={chemical.id} disabled={select1===chemical.id ? true : false}>{chemical.nameOfProduct}</option>)};
+              {chemicals.map((chemical) => (
+                <option
+                  key={chemical.id}
+                  value={chemical.id}
+                  disabled={select1 === chemical.id ? true : false}
+                >
+                  {chemical.nameOfProduct}
+                </option>
+              ))}
+              ;
             </select>
-            <button className="btn" type="button" disabled={select2 ? false : true} onClick={() => handleOpenInfoBox("select2")}>?</button>
+            <button
+              className="btn"
+              type="button"
+              disabled={select2 ? false : true}
+              onClick={() => handleOpenInfoBox('select2')}
+            >
+              ?
+            </button>
           </label>
-            <div className="seznam-chemikalii-controls">
-                <button className="btn" type="submit" disabled={(select1 && select2) ? false : true}>Míchat</button>
-            </div>
+          <div className="seznam-chemikalii-controls">
+            <button
+              className="btn"
+              type="submit"
+              disabled={select1 && select2 ? false : true}
+            >
+              Míchat
+            </button>
+          </div>
         </form>
       </div>
-      {chemicalInfo && <InfoBox name={chemicalInfo.nameOfProduct} chemical={chemicalInfo.nameOfChemical} products={chemicalInfo.otherProducts} use={chemicalInfo.use} specs={chemicalInfo.specification} warning={chemicalInfo.warningSign}/> }
+      {chemicalInfo && (
+        <InfoBox
+          name={chemicalInfo.nameOfProduct}
+          chemical={chemicalInfo.nameOfChemical}
+          products={chemicalInfo.otherProducts}
+          use={chemicalInfo.use}
+          specs={chemicalInfo.specification}
+          warning={chemicalInfo.warningSign}
+        />
+      )}
       {mix ? <MixResult chemicalID1={select1} chemicalID2={select2} /> : null}
     </div>
   );
