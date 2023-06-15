d3a07f5d1627973878b08171b64a0b8bfc189b65
Kamila Semotánová <kamila.semotanova@gmail.com>
2023-06-15 11:47:08 +0200

    Optimize props in chemcircle and chemical

--- src/components/Chemical/index.jsx
+++ src/components/Chemical/index.jsx
@@ -5,12 +5,6 @@ import { InfoBox } from '../InfoBox';
 export const Chemical = ({
   id,
   url,
-  name,
-  chemical,
-  products,
-  use,
-  specs,
-  warnings,
   onTouchStart,
   onTouchMove,
   onTouchEnd,
@@ -33,24 +27,14 @@ export const Chemical = ({
       onTouchMove={onTouchMove}
       onTouchEnd={onTouchEnd}
       onDragStart={onDragStart}
-      className={mixList.includes(id) ? "chemical disabled" : "chemical"}
+      className={mixList.includes(id) ? 'chemical disabled' : 'chemical'}
       style={{
         position: 'absolute',
         transform: `translate(${positionY}px, ${positionX}px )`,
       }}
     >
       <img src={url} alt="product picture" className="chemicalLogo" />
-      {openInfoBox && (
-        <InfoBox
-          id={id}
-          name={name}
-          chemical={chemical}
-          products={products}
-          use={use}
-          specs={specs}
-          warnings={warnings}
-        />
-      )}
+      {openInfoBox && <InfoBox id={id} />}
     </div>
   );
 };
