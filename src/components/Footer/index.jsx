05b1120e730f509c202c5e4d1b05ea0443e86ded
Susanne Krkoska <zuzka.krkoska@seznam.cz>
2023-06-15 23:03:01 +0200

    Add Footer styles

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -4,9 +4,9 @@ import './style.css';
 export const Footer = () => {
   return (
     <footer>
-      <p>© 2023 Neztrať hlavu</p>
-      <a href="https://github.com/KamilaSemotanova/neztrat-hlavu" target="_blank">Repozitář hry</a>
-      <a href='https://about-neztrathlavu.netlify.app/' target="_blank">Stránka o hře: </a>
+      <p className="footer__trademark">© 2023 Neztrať hlavu</p>
+      <a className="footer__link" href="https://github.com/KamilaSemotanova/neztrat-hlavu" target="_blank">Repozitář hry</a>
+      <a className="footer__link" href='https://about-neztrathlavu.netlify.app/' target="_blank">Stránka o hře: </a>
     </footer>
   );
 };
