ebbfa5f4eb9e7ec7ce88d1fba2affa6cd537b7f1
Susanne Krkoska <zuzka.krkoska@seznam.cz>
2023-06-16 01:48:40 +0200

    Add MixResult styles

--- src/components/InfoBox/index.jsx
+++ src/components/InfoBox/index.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import './style.css';
 import { chemicals } from '../../chemicals';
 import { isMobile, isTablet } from 'react-device-detect';
@@ -6,23 +6,40 @@ import { isMobile, isTablet } from 'react-device-detect';
 export const InfoBox = ({ id, onClose }) => {
   const selectedChemical = chemicals.find((chemical) => chemical.id === id);
 
-
   return (
     <div className="infobox__overlay">
-      <div className={isMobile ? "infobox__container mobile" : "infobox__container"}>
+      <div
+        className={
+          isMobile ? 'infobox__container mobile' : 'infobox__container'
+        }
+      >
         <h2 className="infobox__heading">{selectedChemical.nameOfProduct}</h2>
         <div className="infobox__body">
-          <div className="infobox-chemical">
-            <h2 className="infobox__bold">Název chemické sloučeniny:</h2> {selectedChemical.nameOfChemical}
+          <div>
+            <h2 className="infobox__bold">Název chemické sloučeniny:</h2>{' '}
+            {selectedChemical.nameOfChemical}
           </div>
-          <div className="infobox-other"><h2 className="infobox__bold">Také v:</h2> {selectedChemical.otherProducts}</div>
-          <div className="infobox-use"><h2 className="infobox__bold">Využívá se k:</h2> {selectedChemical.use}</div>
-          <div className="infobox-specs"><h2 className="infobox__bold">Jak vypadá: </h2>{selectedChemical.specification}</div>
-          <div className="infobox-warnings">{selectedChemical.warningSign}</div>
+          <div>
+            <h2 className="infobox__bold">Také v:</h2>{' '}
+            {selectedChemical.otherProducts}
+          </div>
+          <div>
+            <h2 className="infobox__bold">Využívá se k:</h2>{' '}
+            {selectedChemical.use}
+          </div>
+          <div>
+            <h2 className="infobox__bold">Jak vypadá: </h2>
+            {selectedChemical.specification}
+          </div>
+          <div>{selectedChemical.warningSign}</div>
         </div>
-        {isMobile ? <div className="infobox__btn-center">
-            <button className="infobox__btn" onClick={() => onClose(false)}>Zpět</button>
-          </div> : null}
+        {isMobile ? (
+          <div className="infobox__btn-center">
+            <button className="infobox__btn" onClick={() => onClose(false)}>
+              Zpět
+            </button>
+          </div>
+        ) : null}
       </div>
     </div>
   );
--- src/components/MixResult/index.jsx
+++ src/components/MixResult/index.jsx
@@ -1,16 +1,24 @@
-import React from "react";
-import { reactionResult } from "./reactions";
+            import React from "react";
+            import { reactionResult } from "./reactions";
+            import "./style.css"
 
-export const MixResult = ({chemicalID1, chemicalID2}) => {
-  const reactionID = chemicalID1.localeCompare(chemicalID2) < 0 ? chemicalID1+chemicalID2 : chemicalID2+chemicalID1
+            export const MixResult = ({chemicalID1, chemicalID2}) => {
+              const reactionID = chemicalID1.localeCompare(chemicalID2) < 0 ? chemicalID1+chemicalID2 : chemicalID2+chemicalID1
 
-  const reaction = reactionResult[reactionID]
+              const reaction = reactionResult[reactionID]
 
-  return (
-    <div className="reactionResult">
-      <h3>{reaction.title}</h3>
-      <p>{reaction.reaction}</p>
-      <p>{reaction.resultInfo}</p>
-    </div>
-  );
-};
+              return (
+                <div className="mixresult__overlay">
+                    <div className="mixresult__container">
+                  <h3 className="mixresult__heading">{reaction.title}</h3>
+                        <div className="mixresult__body">
+                  <p>{reaction.reaction}</p>
+                  <p>{reaction.resultInfo}</p>
+                        </div>
+                        <div className="mixresult__btn-center">
+                        <button className="mixresult__btn">Zpět</button>
+                        </div>
+                    </div>
+                </div>
+              );
+            };
