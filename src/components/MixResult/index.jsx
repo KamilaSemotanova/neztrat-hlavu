2b80ec0e400846bc87b54d6702dddf4903267f4f
Susanne Krkoska <zuzka.krkoska@seznam.cz>
2023-06-17 01:11:23 +0200

    Add animated icon to MixResult

--- src/components/MixResult/index.jsx
+++ src/components/MixResult/index.jsx
@@ -2,6 +2,7 @@
             import { isMobile } from 'react-device-detect';
             import { reactionResult } from "./reactions";
             import "./style.css"
+            import icon from "../../img/icon.gif";
 
             export const MixResult = ({chemicalID1, chemicalID2, handleClick}) => {
               const reactionID = chemicalID1.localeCompare(chemicalID2) < 0 ? chemicalID1+chemicalID2 : chemicalID2+chemicalID1
@@ -11,7 +12,7 @@
               return (
                 <div className={isMobile ? "mixresult__overlay mobile-mixresult__overlay" : "mixresult__overlay"}>
                     <div className={isMobile ? "mixresult__container mobile-mixresult__container" : "mixresult__container"}>
-                  <h3 className="mixresult__heading">{reaction.title}</h3>
+                  <h3 className="mixresult__heading"><img className="mixresult__icon" src={icon}/>{reaction.title}</h3>
                         <div className="mixresult__body">
                   <p>{reaction.reaction}</p>
                   <p>{reaction.resultInfo}</p>
