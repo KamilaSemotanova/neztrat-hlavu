50b9ebcf8dd0cf203dfc1d08af1d38efe97c352d
Kamila Semotánová <kamila.semotanova@gmail.com>
2023-06-17 11:47:38 +0200

    Create randomizer of funfacts

--- src/components/MixResult/index.jsx
+++ src/components/MixResult/index.jsx
@@ -1,26 +1,61 @@
-            import React from "react";
-            import { isMobile } from 'react-device-detect';
-            import { reactionResult } from "./reactions";
-            import "./style.css"
-            import icon from "../../img/icon.gif";
+import React, { useState, useEffect } from 'react';
+import { isMobile } from 'react-device-detect';
+import { reactionResult } from './reactions';
+import './style.css';
+import icon from '../../img/icon.gif';
+import { funfacts } from './funfacts';
 
-            export const MixResult = ({chemicalID1, chemicalID2, handleClick}) => {
-              const reactionID = chemicalID1.localeCompare(chemicalID2) < 0 ? chemicalID1+chemicalID2 : chemicalID2+chemicalID1
+export const MixResult = ({ chemicalID1, chemicalID2, handleClick }) => {
+  const [randomFact, setRandomFact] = useState('');
 
-              const reaction = reactionResult[reactionID]
+  const reactionID =
+    chemicalID1.localeCompare(chemicalID2) < 0
+      ? chemicalID1 + chemicalID2
+      : chemicalID2 + chemicalID1;
 
-              return (
-                <div className={isMobile ? "mixresult__overlay mobile-mixresult__overlay" : "mixresult__overlay"}>
-                    <div className={isMobile ? "mixresult__container mobile-mixresult__container" : "mixresult__container"}>
-                  <h3 className="mixresult__heading"><img className="mixresult__icon" src={icon}/>{reaction.title}</h3>
-                        <div className="mixresult__body">
-                  <p>{reaction.reaction}</p>
-                  <p>{reaction.resultInfo}</p>
-                        </div>
-                        <div className="mixresult__btn-center">
-                        <button className="mixresult__btn" onClick={() => handleClick()}>Zpět</button>
-                        </div>
-                    </div>
-                </div>
-              );
-            };
+  const reaction = reactionResult[reactionID];
+
+  useEffect(() => {
+    const randomNumber = Math.floor(Math.random() * funfacts.length);
+
+    setRandomFact(funfacts[randomNumber]);
+  }, [chemicalID1 && chemicalID2]);
+
+  return (
+    <div
+      className={
+        isMobile
+          ? 'mixresult__overlay mobile-mixresult__overlay'
+          : 'mixresult__overlay'
+      }
+    >
+      <div
+        className={
+          isMobile
+            ? 'mixresult__container mobile-mixresult__container'
+            : 'mixresult__container'
+        }
+      >
+        {reaction.title ? (
+          <>
+            <h3 className="mixresult__heading">
+              <img className="mixresult__icon" src={icon} />
+              {reaction.title}
+            </h3>
+            <div className="mixresult__body--reaction">
+              <p>{reaction.reaction}</p>
+              <p>{reaction.resultInfo}</p>
+            </div>
+          </>
+        ) : (
+          <p className="mixresult__body--nothing">{randomFact}</p>
+        )}
+        <div className="mixresult__btn-center">
+          <button className="mixresult__btn" onClick={() => handleClick()}>
+            Zpět
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
