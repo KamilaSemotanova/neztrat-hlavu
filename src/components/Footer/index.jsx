39d26cfdb3cfff647771ebaf4386027e8eee392a
Kamila Semotánová <kamila.semotanova@gmail.com>
2023-06-18 11:28:12 +0200

    Open links directly

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -4,9 +4,19 @@ import './style.css';
 export const Footer = () => {
   return (
     <footer>
-      <a className="footer__link" href="https://github.com/KamilaSemotanova/neztrat-hlavu" target="_blank">Repozitář hry</a>
-      <a className="footer__link" href='https://about-neztrathlavu.netlify.app/' target="_blank">Stránka o hře: </a>
-          <p className="footer__trademark">© 2023 Neztrať hlavu</p>
+      <a
+        className="footer__link"
+        href="https://github.com/KamilaSemotanova/neztrat-hlavu"
+      >
+        Repozitář hry
+      </a>
+      <a
+        className="footer__link"
+        href="https://about-neztrathlavu.netlify.app/"
+      >
+        Stránka o hře:{' '}
+      </a>
+      <p className="footer__trademark">© 2023 Neztrať hlavu</p>
     </footer>
   );
 };
